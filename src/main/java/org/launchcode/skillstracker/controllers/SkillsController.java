8bbb5e40dc6a6d4835fbbe9a55d28afa929e24fd
Emily Spradling <spradling.ed@gmail.com>
2022-10-10 21:17:53 -0500

    studio finished

--- src/main/java/org/launchcode/skillstracker/controllers/SkillsController.java
+++ src/main/java/org/launchcode/skillstracker/controllers/SkillsController.java
@@ -2,6 +2,8 @@ package org.launchcode.skillstracker.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -18,9 +20,23 @@ public class SkillsController {
     @GetMapping("form")
     @ResponseBody
     public String chooseSkills(){
-        return ("<form>Name: <input type ='text'</form><br>" +
-                "<label for = 'favorite'>My favorite language is: </label><select name='favorite'> <option value='javascript'>JavaScript</option> <option value='java'>Java</option><option value='python'>Python</option></select><br>" +
-                "<label for = 'second-favorite'>My second favorite language is: </label><select name='second-favorite'> <option value='javascript'>JavaScript</option> <option value='java'>Java</option><option value='python'>Python</option></select><br>") +
-                "<label for = 'third-favorite'>My third favorite language is: </label><select name='third-favorite'> <option value='javascript'>JavaScript</option> <option value='java'>Java</option><option value='python'>Python</option></select><br>";
+        return ("<form method='post'> + " +
+                "Name: <input type ='text' name = 'name'><br>" +
+                "<br><label for = 'favorite'>My favorite language is: </label><select name='favorite'> <option value='javascript'>JavaScript</option> <option value='java'>Java</option><option value='python'>Python</option></select><br>" +
+                "<label for = 'secondFavorite'>My second favorite language is: </label><select name='secondFavorite'> <option value='javascript'>JavaScript</option> <option value='java'>Java</option><option value='python'>Python</option></select><br>" +
+                "<label for = 'thirdFavorite'>My third favorite language is: </label><select name='thirdFavorite'> <option value='javascript'>JavaScript</option> <option value='java'>Java</option><option value='python'>Python</option></select><br>" +
+                "<input type = 'submit' value = 'Submit'/></form>");
+    }
+
+    @PostMapping(value="form")
+    @ResponseBody
+    public String skillsResults(@RequestParam String name, @RequestParam String favorite, @RequestParam String secondFavorite, @RequestParam String thirdFavorite){
+        String html = "<h1>" + name + "</h1>"+
+                "<ol>" +
+                "<li>" + favorite + "</li>" +
+                "<li>" + secondFavorite + "</li>" +
+                "<li>" + thirdFavorite + "</li>" +
+                "</ol>";
+        return html;
     }
 }
